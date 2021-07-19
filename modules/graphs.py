c00c7a4b007fce6b75e28624f333101be5e53853
Blah029 <81973454+Blah029@users.noreply.github.com>
2021-07-19 16:34:00 +0530

    Update graphs.py

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plotBestFit(x,y,degree=1,plotLabel=None, plotColour="tab:blue"):
+def plotBestFit(x,y, degree=1, plotLabel=None, plotColour="tab:blue"):
     """Plots the best fit curve of given degree to fit the given data"""
     m,b = np.polyfit(x,y,degree)
     plt.plot(x,y,"o", color=plotColour)
