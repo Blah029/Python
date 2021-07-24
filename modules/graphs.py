a7882f029bf5a3a37f8f24a72ccaa5cc295a1177
Blah029 <81973454+Blah029@users.noreply.github.com>
2021-07-24 19:50:23 +0530

    Update graphs.py

--- modules/graphs.py
+++ modules/graphs.py
@@ -26,6 +26,15 @@ def setGrid(xLabel=None, yLabel=None, title=None):
     plt.grid(which="minor", alpha=0.25)
     plt.minorticks_on()
 
+def pointLabels(x,y):
+    """Plot data points"""
+
+    for i, j in zip(x, y):
+        plt.text(i, j+1,("({:.0f},{:.0f})".format(i,j)), backgroundcolor="white", alpha=0.5)
+
+def plotPoints(x,y, plotColour=None):
+    plt.plot(x,y,"o", color=plotColour)
+
 def plotBestFit(x,y, matchY=False, degree=1, plotLabel=None, plotColour="tab:blue"):
     """Plots the best fit curve of given degree to fit given numpy arrays"""
     m,c = np.polyfit(x,y,degree)
