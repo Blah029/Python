8b5dd0fa259ebd336748ba4fef75d7c7ae25e610
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-10-25 00:49:48 +0530

    Add files via upload

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,16 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
+"""Tools for plotting graphs with pyplot
 
-"""
 figure template
-
+# Configure axes
 fig, ax = plt.subplots(_int_)
 ax.set_xscale(_string_)
 ax.set_yscale(_string_)
 ax.set_xlim(_int_,_int_)
 ax.set_ylim(_int_,_int_)
-
-# replace plt with ax[_int_] for subplots
+# Plot. Replace plt with ax[_int_] for subplots.
 plt.plot(_array_,_array_, label=_string_, color=_string_)
 plt.xlabel(_string_)
 plt.ylabel(_string_)
@@ -21,9 +18,12 @@ plt.grid(which="minor", alpha=0.25))
 plt.minorticks_on()
 plt.show()
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def setGrid(xLabel=None, yLabel=None, title=None):
-    """Configures axis labels, title and other roperties"""
+    """Configure axis labels, title and other roperties"""
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
     plt.title(title)
@@ -32,31 +32,33 @@ def setGrid(xLabel=None, yLabel=None, title=None):
     plt.grid(which="minor", alpha=0.25)
     plt.minorticks_on()
 
+
 def pointLabels(x,y):
     """Plot data points"""
-
     for i, j in zip(x, y):
-        plt.text(i, j+1,("({:.0f},{:.0f})".format(i,j)), backgroundcolor="white", alpha=0.5)
+        plt.text(i, j+1,("({:.0f},{:.0f})".format(i,j)),
+                 backgroundcolor="white", alpha=0.5)
+
 
 def plotPoints(x,y, plotColour=None):
+    """Mark points on the graph"""
     plt.plot(x,y,"o", color=plotColour)
 
+
 def plotBestFitPoly(x,y, degree=1, plotLabel=None, plotColour="tab:blue"):
-    """Plots the best fiting polynomial curve of given degree to fit given numpy arrays"""
-    
+    """Plot the best fiting polynomial curve of given degree to fit given numpy arrays"""
     coefficients = np.polyfit(x,y,degree)
     xAxis = np.linspace(x[0],x[-1],(x[-1]-x[0]+1)*100)
-    yFitted = np.zeros(len(xAxis))
-    
+    yFitted = np.zeros(len(xAxis))    
     for i in range(degree+1):
         yFitted += coefficients[-i-1]*xAxis**i
 
     plt.plot(xAxis,yFitted, label=plotLabel, color=plotColour)
     plt.plot(x,y,"o", color=plotColour)
 
+
 def plotBestFitLog(x,y, plotLabel=None, plotColour="tab:blue"):
-    """Plots the best fiting polynomial curve of given degree to fit given numpy arrays"""
-    
+    """Plot the best fiting polynomial curve of given degree to fit given numpy arrays"""
     coefficients = np.polyfit(np.log(x),y,1)
     xAxis = np.linspace(x[0],x[-1],(x[-1]-x[0]+1)*100)
     yFitted = coefficients[0]*np.log(xAxis) + coefficients[1]
--- modules/myMath.py
+++ modules/myMath.py
@@ -1,122 +1,93 @@
 def factors(number):
-    """Returns the factors of number as a list"""
-    fact=[]
-
+    """Return the factors of number as a list"""
+    fact = []
     for i in range(1, number+1):
-
-        if number%i==0:
-            fact+=[i]
+        if number % i == 0:
+            fact += [i]
             
     return fact
 
+
 def checkIfPrime(number):
-    """Returns True if number is prime"""
-    isPrime=False
-    f=[]
-
-    if number==1:
-        isPrime=False
-
+    """Return True if number is prime"""
+    isPrime = False
+    f = []
+    if number == 1:
+        isPrime = False
     else:
-
         for j in range(1,number+1):
-
-            if number%j==0:
-                f+=[j]
-
-            if len(f)<=2:
-                isPrime=True
-
+            if number % j == 0:
+                f += [j]
+            if len(f) <= 2:
+                isPrime = True
             else:
-                isPrime=False
+                isPrime = False
 
     return isPrime
 
+
 def primeNumbers(upperLimit, lowerLimit=1):
-    """Returns the prime numbers betwerrn upperLimit and lowerLimit"""
-
-    if lowerLimit==1:
-        primeNums=[2]
-        a=2
-        prime=None
-
+    """Return the prime numbers betwerrn upperLimit and lowerLimit"""
+    if lowerLimit == 1:
+        primeNums = [2]
+        a = 2
+        prime = None
         for b in range(upperLimit-1):
-
             for c in primeNums:
-
-                if a%c==0:
+                if a % c == 0:
                     prime=False
                     break
-
                 else:
                     prime=True
-
-            if prime==True:
-                primeNums+=[a]
-
+            if prime == True:
+                primeNums += [a]
             a+=1
-
     else:
-        f=[]
-        primeNums=[]
-
+        f =[]
+        primeNums = []
         for i in range(lowerLimit+1,upperLimit+1):	
-
             for j in range(1,i+1):
-
-                if i%j==0:
-                    f+=[j]
-
-            if len(f)<=2:
-                    primeNums+=[i]
-                    
+                if i % j == 0:
+                    f += [j]
+            if len(f) <= 2:
+                    primeNums += [i]
             f=[]
 
     return primeNums
 
+
 def primeFactors(number):
-    """Returns the prime factors of number as a list"""
+    """Return the prime factors of number as a list"""
     pf=[]
     pn=primeNumbers(number, 1)
-
     while number > 1:
-       
         for b in pn:
-
-            while number%b==0:
+            while number % b == 0:
                 pf.append(b)
-                number=int(number/b)
-            
-        number-=1
+                number = int(number/b)
+        number -= 1
 
     return pf
 
+
 def lcm(numbers):
-    """Returns the least common multiple of numbers. (Must be <class 'list'>)"""
-    
-    if str(type(numbers))=="<class 'list'>":
+    """Return the least common multiple of numbers. (Must be <class 'list'>)"""
+    if str(type(numbers)) == "<class 'list'>":
         numbers.sort()
-        pn=primeNumbers(numbers[-1])
-        all_pf=[]
-        occur={}
-        answer=1
-
+        pn = primeNumbers(numbers[-1])
+        all_pf = []
+        occur = {}
+        answer = 1
         for e in pn:
-            occur[e]=0
-
+            occur[e] = 0
         for a in numbers:
             all_pf.append(primeFactors(a))
-    
-
         for b in all_pf:
-
             for c in pn:
-        
-                if b.count(c)>occur[c]:
-                    occur[c]=b.count(c)
-
+                if b.count(c) > occur[c]:
+                    occur[c] = b.count(c)
         for d in occur:
-            answer*=d**occur[d]
+            answer* = d**occur[d]
 
         return answer
 
