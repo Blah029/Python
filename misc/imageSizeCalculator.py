ccd1783da5622134e61ca4e55bf3205a51d3066c
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-12-14 17:09:11 +0530

    Add imageSizeCalculator.py

---
+++ misc/imageSizeCalculator.py
@@ -0,0 +1,21 @@
+"""A script to calculate a consistent image size based on aspect ratio for reports"""
+
+normalWidth = 4
+normalHeight = 3
+normalAspectRatio = normalHeight/normalWidth
+
+
+def calculateLeftMargin(width, height):
+    """Returns the left margin to be set for a centered image in inches"""
+    area = width*height
+    ratio = (area/(8.75**2*normalAspectRatio))**0.5
+    normalisedWidth = width/ratio
+    return round((17.5-normalisedWidth))/2
+
+
+def main():
+    print(calculateLeftMargin(3,4))
+
+
+if __name__ == "__main__":
+    main()
