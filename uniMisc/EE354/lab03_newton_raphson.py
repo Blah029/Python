26148708608dcb194692f48219be360191b0e7ca
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-02-15 19:58:05 +0530

    Update lab03_newton_raphson.py

--- uniMisc/EE354/lab03_newton_raphson.py
+++ uniMisc/EE354/lab03_newton_raphson.py
@@ -11,7 +11,6 @@ y23 = 16.0256-80.1282j
 V = np.array([[1],
               [1.04],
               [1.02]])
-D = np.zeros((3,1))
 P = np.array([[3.6],
               [0],
               [2]])
@@ -19,6 +18,7 @@ Q = np.array([[1.1833],
               [0],
               [0]])
 N = np.shape(V)[0]
+D = np.zeros((N,1))
 
 
 def vComp(index):
@@ -109,7 +109,64 @@ print("--------J")
 JP = np.concatenate((PD,PV), axis=1)
 JQ = np.concatenate((QD,QV), axis=1)
 J = np.concatenate((JP,JQ))
+JInverse = np.linalg.inv(J)
 print(J)
+print(JInverse)
+#change in P,Q
+print("--------P1,P3 inital")
+PInitial = np.zeros((N,1))
+for i in [0,2]:
+    for j in range(N):
+        # print('\t',i+1,j+1,'\t',V[i,0],np.abs(Y[i,j]),V[j,0],'\t',D[i,0],-D[j,0],-np.angle(Y[i,j])/math.pi*180)
+        # print('\t',i+1,j+1,'\t',np.abs(V[i,0]*Y[i,j]*V[j,0]),math.cos(D[i,0]-D[j,0]-np.angle(Y[i,j])))
+        # print('\t',i+1,j+1,'\t',np.abs(V[i,0]*Y[i,j]*V[j,0]) * math.cos(D[i,0]-D[j,0]-np.angle(Y[i,j])))
+        PInitial[i,0] += np.abs(V[i,0]*Y[i,j]*V[j,0]) * math.cos(D[i,0]-D[j,0]-np.angle(Y[i,j]))
+print(PInitial)
+print("--------Q1 inital")
+QInitial = np.zeros((N,1))
+for i in [0]:
+    for j in range(N):
+        # print('\t',i+1,j+1,'\t',V[i,0],np.abs(Y[i,j]),V[j,0],'\t',D[i,0],-D[j,0],-np.angle(Y[i,j])/math.pi*180)
+        # print('\t',i+1,j+1,'\t',np.abs(V[i,0]*Y[i,j]*V[j,0]),math.sin(D[i,0]-D[j,0]-np.angle(Y[i,j])))
+        # print('\t',i+1,j+1,'\t',np.abs(V[i,0]*Y[i,j]*V[j,0]) * math.sin(D[i,0]-D[j,0]-np.angle(Y[i,j])))
+        QInitial[i,0] += np.abs(V[i,0]*Y[i,j]*V[j,0]) * math.sin(D[i,0]-D[j,0]-np.angle(Y[i,j]))
+print(QInitial)
+print("--------PQ delta")
+PDelta = P-PInitial
+QDelta = Q-QInitial
+PQDelta = np.row_stack((PDelta[0],PDelta[2],QDelta[0]))
+print(PQDelta)
+#voltage values
+print("--------DV delta")
+DVDelta = np.matmul(JInverse,PQDelta)
+print(DVDelta)
+print("--------DV")
+DVInital = np.row_stack((D[0,0],D[2,0],V[0,0]))
+DV = DVInital + DVDelta
+print(DVInital)
+print(DV)
+
+#fast decouple
+#jacobian matrix
+print('\n',"--------J2", sep="")
+JP2 = np.concatenate((PD,np.zeros((2,1))), axis=1)
+JQ2 = np.concatenate((np.zeros((1,2)),QV), axis=1)
+J2 = np.concatenate((JP2,JQ2))
+J2Inverse = np.linalg.inv(J2)
+print(J2)
+print(J2Inverse)
+#voltage values
+print("--------DV delta")
+DVDelta2 = np.matmul(J2Inverse,PQDelta)
+print(DVDelta2)
+print("--------DV")
+DVInital = np.row_stack((D[0,0],D[2,0],V[0,0]))
+DV2 = DVInital + DVDelta2
+print(DVInital)
+print(DV2)
+
+
+
 
 
     
