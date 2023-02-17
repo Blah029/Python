b9546a4ac3f858f7ac0a37fddddb58570cfaacf6
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-02-17 10:26:56 +0530

    Update tr400ImageSize.py

--- uniMisc/tr400ImageSize.py
+++ uniMisc/tr400ImageSize.py
@@ -6,12 +6,12 @@ refArea = 54.1875 #landscape 4:3, 8.5 inch horizontal
 def resize(width,height):
     global refArea
     aspRatio = width/height
-    print(round(math.sqrt(refArea*aspRatio)*2,0)/2)
+    # print(round(math.sqrt(refArea*aspRatio)*2,0)/2)
     return round((17.5-math.sqrt(refArea*aspRatio)))/2
 
 
 def main():
-    print(resize(4000,1087))
+    print(resize(425,268))
 
 
 if __name__ == "__main__":
