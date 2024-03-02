02342e142c94ff5e644728e55b76d60059bf0732
Blah029 <81973454+Blah029@users.noreply.github.com>
2024-03-02 23:53:15 +0530

    Update image_size_calculator.py
    add downscale resolution()

--- misc/image_size_calculator.py
+++ misc/image_size_calculator.py
@@ -1,4 +1,6 @@
-"""A script to calculate a consistent image size based on aspect ratio for reports"""
+"""A script to calculate a consistent image size 
+based on aspect ratio for reports
+"""
 
 width_factor = 4
 height_factor = 3
@@ -13,8 +15,22 @@ def calculate_left_margin(width, height):
     return round((17.5-width_normalised))/2
 
 
+def downscale_resolution(width, height, target_width=1920, target_height=1080):
+    """Returns the resolution after downscaling to fill 1920 x 1080 window"""
+    scaled_width = target_height/height * width
+    scaled_height = target_width/width * height
+    # Wider than target
+    if scaled_width >= target_width:
+        scaled_height = target_height
+    # Taller than target
+    elif scaled_height >= target_height:
+        scaled_width = target_width
+    return f"{scaled_width:.0f} {scaled_height:.0f}"
+
+
 def main():
-    print(calculate_left_margin(401,192))
+    # print(calculate_left_margin(401,192))
+    print(downscale_resolution(1536,864))
 
 
 if __name__ == "__main__":
